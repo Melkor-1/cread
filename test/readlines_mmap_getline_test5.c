78f99f8f53c5f314822c7e7fdd2d7ad1e006176f
Melkor-1 <140725427+Melkor-1@users.noreply.github.com>
2024-05-30 16:13:40 +0000

    Test the actual functionality of the read routines.
    * Is the line count the same that wc reports?
    * Is the byte count the same that wc reports?
    * Are the contents of the lines the same as those returned by fgets?

---
+++ test/readlines_mmap_getline_test5.c
@@ -0,0 +1,45 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "acutest.h"
+
+#include "../src/FileBuf.h"
+#include "../src/readlines_getline.c"
+#include "../src/readlines_mmap_getline.c"
+
+void test_readlines_mmap_getline_correctness(void)
+{
+    /* The lines and line lengths are at most 1024 in sample1.txt. */
+    char buf[1024];
+    char *lines[1024];
+    size_t line_count = 0;
+    FILE *const fp = fopen("test/sample.txt", "r");
+
+    assert(fp);
+
+    while (fgets(buf, sizeof buf, fp)) {
+        buf[strcspn(buf, "\n")] = '\0';
+        assert(lines[line_count] = strdup(buf));
+        ++line_count;
+    }
+    
+    rewind(fp);
+
+    FileBuf fbuf = {};
+
+    assert(readlines_mmap_getline(fp, &fbuf));
+    assert(fbuf.count == 555);
+    assert(fbuf.size == 120635);
+
+    for (size_t i = 0; i < fbuf.count; ++i) {
+        assert(strcmp(lines[i], fbuf.lines[i].line) == 0);
+    }
+
+    fclose(fp);
+}
+
+TEST_LIST = {
+    { "test_readlines_mmap_getline_correctness", test_readlines_mmap_getline_correctness },
+    { nullptr, nullptr },
+};
